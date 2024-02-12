713c1cad63190e0878cd2913edbf5c4daac97540
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-02-13 10:58:48 +1300

    Code base cleaned up, and add expense card improved.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,103 +1,148 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
+import { v4 as uuidv4 } from'uuid';
 
 function App() {
-
-  const [expenses, setExpenses] = useState([
-    // {
-    //   id: 0,
-    //   name: "Dairy",
-    //   amount: 24.00,
-    //   date: "2024-02-12",
-    //   category: "Food"
-    // }
-  ]);
-  const [budget, setBudget] = useState(100);
-
-  const calculateTotalExpenses = () => {
-    let totalExpenses = 0;
-    expenses.map(expense => (
-      totalExpenses += expense.amount
-    ));
-    return (totalExpenses);
-  }
-
+  // Preparing date related variables
+  const currentDay = new Date().getDay();
   const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
 
+  // Month names for prints to screen based on currentMonth
   const monthNames = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
   ];
 
+  // Returns the month index of a date stored as a string
+  const getMonthFromDate = (date) => {
+    return parseInt(date.split('-')[1]) - 1;
+  }
+
+  // Dictionary look up, each year contains an array, with each element being an array of expenses for each month
+  // Expense: id, name, amount, date, category
+  const [expenses, setExpenses] = useState({
+    2024:
+    [
+      [
+        { id: 0, name: "Food", amount: 100.00, date: "2024-01-13" }
+      ],
+      [
+        { id: 1, name: "Food", amount: 100.00, date: "2024-02-13" }
+      ]
+    ]
+  });
+
+  // Dictionary look up, each year contains array with budget for each month
+  const [budget, setBudget] = useState({
+    2024: [100, 200],
+  })
+
+  // Calculates total expenses for current month
+  const calculateTotalExpenses = () => {
+    return expenses[currentYear][currentMonth].reduce((total, expense) => {
+      return total + (getMonthFromDate(expense.date) === currentMonth ? expense.amount : 0);
+    }, 0);
+  }
+
+  // Is user adding an expense
   const [addingExpense, setAddingExpense] = useState(false);
 
+  // Toggle whether or not a user is adding an expense
   const handleAddExpensePress = () => {
     setAddingExpense(!addingExpense);
   }
 
+  // Variables for users input of new expenses
   const [expenseName, setExpenseName] = useState('');
   const [expenseAmount, setExpenseAmount] = useState('');
 
+  // Adds inputed expense to array
   const handleAddExpense = () => {
     // Validate input
     if (expenseName.trim() === '' || isNaN(expenseAmount)) {
       alert('Please enter a valid expense name and amount.');
       return;
     }
-
-    // Convert amount to a number
-    const amount = parseFloat(expenseAmount);
-
+    
     // Create a new expense object
     const newExpense = {
+      id: uuidv4(),
       name: expenseName,
-      amount: amount
+      amount: parseFloat(expenseAmount),
+      date: new Date().toISOString().split('T')[0]
     };
+
+    // Update the expenses array for the current year and month
+    setExpenses(prevExpenses => {
+      const updatedExpenses = { ...prevExpenses };
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth();
+
+      if (!updatedExpenses[currentYear]) {
+        updatedExpenses[currentYear] = {};
+      }
+
+      if (!updatedExpenses[currentYear][currentMonth]) {
+        updatedExpenses[currentYear][currentMonth] = [];
+      }
+
+      // Add the new expense to the existing array
+      updatedExpenses[currentYear][currentMonth].push(newExpense);
+
+      return updatedExpenses;
+    });
 
     // Clear input fields
     setExpenseName('');
     setExpenseAmount('');
 
-    // Pass the new expense to the parent component
-    setExpenses([...expenses, newExpense]);
     handleAddExpensePress();
   };
 
   return (
     <div className="App">
-      <div className="Budget">
+      {/* Top section of the app containing current months budget */}
+      <header>
         <p>{monthNames[currentMonth]} Budget</p>
-        <h2 className={`${budget - calculateTotalExpenses() < 0 ? 'negative' : ''}`}>${(budget - calculateTotalExpenses()).toFixed(2)}</h2>
-      </div>
-      <div  className={`ExpenseList ${addingExpense ? 'unfocus' : ''}`}>
+        <h1 className={`${budget[currentYear][currentMonth] - calculateTotalExpenses() < 0 ? 'negative' : 'positive'}`}>${(budget[currentYear][currentMonth] - calculateTotalExpenses()).toFixed(2)}</h1>
+      </header>
+
+      {/* Expense list card */}
+      <div className={`ExpenseList ${addingExpense ? 'unfocus' : ''}`}>
         <div className="ExpenseHeading">
-          <h3>{monthNames[currentMonth]} {new Date().getFullYear()}</h3>
-          <p>{calculateTotalExpenses()}</p>
+          <h2>{monthNames[currentMonth]} {new Date().getFullYear()}</h2>
+          <p className="negative">{calculateTotalExpenses()}</p>
         </div>
-        {expenses.map(expense => (
-          <div className="Expense">
+
+        {/* Loop through each expense for the current year and month, and display */}
+        {expenses[currentYear][currentMonth].map(expense => (
+          <div key={expense.id} className="Expense">
             <p className="Name">{expense.name}</p>
-            <p className="Amount">{expense.amount}</p>
+            <p className="Amount negative">{expense.amount}</p>
           </div>
         ))}
+        
         <button onClick={handleAddExpensePress} className="AddExpenseBtn">+</button>
       </div>
+
+      {/* Overlay card, allowing user to input new expenses */}
       <div className={`AddExpense ${addingExpense ? 'active' : ''}`}>
-          <button onClick={handleAddExpensePress} className="Close">x</button>
-          <input
-            type="text"
-            placeholder="Expense name"
-            value={expenseName}
-            onChange={(e) => setExpenseName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Expense amount"
-            value={expenseAmount}
-            onChange={(e) => setExpenseAmount(e.target.value)}
-          />
-          <button onClick={handleAddExpense} className="Close">+</button>
+        <div className="ExpenseForm">
+          <div className="FormHeader">
+            <button onClick={handleAddExpensePress} className="Close">x</button>
+            <h3>Add Expense</h3>
+          </div>
+          {/* Input fields for new expense */}
+          <div className="InputGroup">
+            <p className="dollar-sign">$</p>
+            <input type="text" inputMode="numeric" pattern="\d+(\.\d{2})?" placeholder="0.00" value={expenseAmount} onChange={(e) => setExpenseAmount(e.target.value)} />
+          </div>
+          <input type="text" placeholder="Expense name" value={expenseName} onChange={(e) => setExpenseName(e.target.value)} /><br />
+
+          <button onClick={handleAddExpense} className="Submit">Add</button>
+        </div>
       </div>
     </div>
   );
